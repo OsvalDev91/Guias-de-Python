1502bcc4d5955ba3282f4df95c8c848939a9c854
Osvaldo Guzman <2505202015@mail.utec.edu.sv>
2023-05-08 11:38:17 -0600

    Se cambio por segunda ves el resultado a entero

--- practica 04/ejercicio02.py
+++ practica 04/ejercicio02.py
@@ -6,7 +6,7 @@ resultado = 1
 if exponente >= 0:
     for i in range(int(exponente)):
         resultado *= numero
-        print(numero, "elevado a la", exponente, "es igual a %.2f"% resultado)
+        print(numero, "elevado a la", exponente, "es igual a %.0f"% resultado)
 
 elif numero or exponente <0:
     resultado = -(1/numero)**exponente
